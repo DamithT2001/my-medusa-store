bd0a09c0f75e7579f531670b6a34c1f7978715e5
DamithT <damith.builtapps@gmail.com>
2026-04-29 14:13:37 +0530

    feat: add tags field as JSONB type in post table and update migration script

--- src/modules/blog/migrations/Migration20260428120000.ts
+++ src/modules/blog/migrations/Migration20260428120000.ts
@@ -2,9 +2,32 @@ import { Migration } from "@medusajs/framework/mikro-orm/migrations"
 
 export class Migration20260428120000 extends Migration {
   async up(): Promise<void> {
-    this.addSql(
-      'create table if not exists "post" ("id" text not null, "title" text not null, "excerpt" text not null default \'\', "slug" text not null, "content" text not null default \'\', "image" text not null default \'\', "thumbnail" text not null default \'\', "tags" text not null default \'\', "author" text not null default \'\', "category" text not null default \'\', "date" text not null default \'\', "status" text not null default \'draft\', "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "post_pkey" primary key ("id"));'
-    )
+    this.addSql(`
+      create table if not exists "post" (
+        "id" text not null,
+        "title" text not null,
+        "excerpt" text not null default '',
+        "slug" text not null,
+        "content" text not null default '',
+        "image" text not null default '',
+        "thumbnail" text not null default '',
+        "tags" jsonb null,
+        "author" text not null default '',
+        "category" text not null default '',
+        "published_at" timestamptz null,
+        "status" text check ("status" in ('draft', 'published')) not null default 'draft',
+        "created_at" timestamptz not null default now(),
+        "updated_at" timestamptz not null default now(),
+        "deleted_at" timestamptz null,
+        constraint "post_pkey" primary key ("id"),
+        constraint "post_slug_unique" unique ("slug")
+      );
+    `)
+
+    this.addSql(`
+      create index if not exists "IDX_post_slug"
+      on "post" ("slug");
+    `)
   }
 
   async down(): Promise<void> {
