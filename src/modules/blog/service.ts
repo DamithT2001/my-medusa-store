0f5ffaa7a7d534854ecd316c099c6ae5b4d9ff2a
DamithT <damith.builtapps@gmail.com>
2026-04-29 14:04:40 +0530

    feat: implement tag normalization and update post payload handling in BlogModuleService

--- src/modules/blog/service.ts
+++ src/modules/blog/service.ts
@@ -1,8 +1,36 @@
 import { MedusaService } from "@medusajs/framework/utils"
 import Post from "./models/post"
 
+const normalizeTags = (tags?: string[] | string | null) => {
+  if (!tags) {
+    return []
+  }
+
+  const rawTags = Array.isArray(tags) ? tags : tags.split(",")
+  const normalizedTags = rawTags
+    .map((tag) => tag.trim())
+    .filter(Boolean)
+
+  return Array.from(new Set(normalizedTags))
+}
+
 class BlogModuleService extends MedusaService({
   Post,
-}) {}
+}) {
+  protected normalizePostPayload<T extends { tags?: string[] | string | null }>(payload: T) {
+    return {
+      ...payload,
+      tags: normalizeTags(payload.tags),
+    }
+  }
+
+  async createPosts(data: Parameters<typeof super.createPosts>[0]) {
+    return super.createPosts(this.normalizePostPayload(data))
+  }
+
+  async updatePosts(data: Parameters<typeof super.updatePosts>[0]) {
+    return super.updatePosts(this.normalizePostPayload(data))
+  }
+}
 
 export default BlogModuleService
